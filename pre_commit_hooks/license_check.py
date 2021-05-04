03ccef787f1760da6b516f8c47fa72dae378a596
Nicolas Byl <nicolas.byl@codecentric.de>
2021-05-04 14:38:35 +0200

    feat: check against allowed licenses

--- pre_commit_hooks/license_check.py
+++ pre_commit_hooks/license_check.py
@@ -1,15 +1,75 @@
 # -*- coding: utf-8 -*-
 import argparse
 import sys
-from os.path import abspath
+from builtins import dict
+from json import loads
+from os.path import abspath, exists
 from pathlib import Path
+from subprocess import run
+from typing import List
 
 
-def get_pipenv_directories(filenames):
+def remove_duplicates(values: List[str]) -> List[str]:
+    return list(dict.fromkeys(values))
+
+
+def get_pipenv_directories(filenames) -> List[str]:
     directories = []
     for filename in filenames:
         directories.append(abspath(Path(filename).parent.absolute()))
-    return list(dict.fromkeys(directories))
+    return remove_duplicates(directories)
+
+
+def parse_licenses(output) -> List[str]:
+    values = loads(output)
+    licenses = []
+
+    for license_structure in values:
+        licenses.append(license_structure['License'])
+
+    return remove_duplicates(licenses)
+
+
+def extract_installed_licenses(directory) -> List[str]:
+    run("pipenv run pip install 'pip-licenses==3.3.1'", check=True, cwd=directory, shell=True)
+    result = run('pipenv run pip-licenses --format=json', capture_output=True, check=True, cwd=directory, shell=True,
+                 text=True)
+    return parse_licenses(result.stdout)
+
+
+ALLOW_LIST_FILE = '.licenses-allowed-pipenv'
+
+
+def get_allow_list_path(directory: str) -> str:
+    return str(Path(directory, ALLOW_LIST_FILE).absolute())
+
+
+def load_allow_list(directory) -> List[str]:
+    list_path = get_allow_list_path(directory)
+    if not exists(list_path):
+        return []
+
+    with open(list_path) as list_file:
+        lines = list_file.readlines()
+        return [line.strip() for line in lines]
+
+
+def find_unallowed_licenses(used_licenses: List[str], allow_list: List[str]) -> List[str]:
+    return list(set(used_licenses) - set(allow_list))
+
+
+def print_license_warning(directory: str, unallowed_licenses: List[str]):
+    unallowed_licenses.sort()
+    print('**************************************************************')
+    print(f'Not all license used by pipenv in directory {directory} are using allowed licenses.')
+    print()
+    print('If you want to allow these licenses, please put the following lines into')
+    print(f'the allow list file: {get_allow_list_path(directory)}: ')
+    print('---')
+    for unallowed_license in unallowed_licenses:
+        print(unallowed_license)
+    print('---')
+    print('**************************************************************')
 
 
 def main(argv=None):
@@ -17,9 +77,20 @@ def main(argv=None):
     parser.add_argument('filenames', nargs='*', help='filenames to check')
     args = parser.parse_args(argv)
 
-    print(args.filenames)
-
     return_code = 0
+
+    directories = get_pipenv_directories(args.filenames)
+
+    for directory in directories:
+        print('**************************************************************')
+        print(f'Starting scan in {directory}...')
+        used_licenses = extract_installed_licenses(directory)
+        allow_list = load_allow_list(directory)
+        unallowed_licenses = find_unallowed_licenses(used_licenses, allow_list)
+        if len(unallowed_licenses) > 0:
+            return_code = 1
+            print_license_warning(directory, unallowed_licenses)
+
     return return_code
 
 
--- tests/test_license_check.py
+++ tests/test_license_check.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
 from pathlib import Path
 
-from pre_commit_hooks.license_check import get_pipenv_directories
+from pre_commit_hooks.license_check import get_pipenv_directories, remove_duplicates, parse_licenses, \
+    find_unallowed_licenses
+
+
+def test_remove_duplicates():
+    list = ['a', 'b', 'c', 'a', 'c']
+    consolidated_list = remove_duplicates(list)
+    assert consolidated_list == ['a', 'b', 'c']
 
 
 def test_get_pipenv_directories():
@@ -11,3 +18,39 @@ def test_get_pipenv_directories():
     assert len(directories) == 2
     assert directories == [str(Path('.').absolute()),
                            str(Path('.', 'deployment').absolute())]
+
+
+def test_parse_licenses():
+    stdout = '''[
+      {
+        "License": "BSD License",
+        "Name": "starlette",
+        "Version": "0.14.1"
+      },
+      {
+        "License": "MIT License",
+        "Name": "urllib3",
+        "Version": "1.26.4"
+      },
+      {
+        "License": "BSD License",
+        "Name": "uvicorn",
+        "Version": "0.13.3"
+      },
+      {
+        "License": "MIT License",
+        "Name": "zipp",
+        "Version": "3.4.1"
+      }
+    ]
+    '''
+    licenses = parse_licenses(stdout)
+    assert licenses == ['BSD License', 'MIT License']
+
+
+def test_find_unallowed_licenses():
+    allowed_licenses = ['BSD License', 'MIT License']
+    used_licenses = ['BSD License', 'GPL', 'MIT License']
+
+    unallowed_licenses = find_unallowed_licenses(used_licenses, allowed_licenses)
+    assert unallowed_licenses == ['GPL']
