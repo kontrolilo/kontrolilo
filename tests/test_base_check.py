54124ce67459e517a53a9ec96fdb7af32c5e4945
Nicolas Byl <nicolas.byl@codecentric.de>
2021-05-06 16:44:19 +0200

    refactor: move tests for superclass into separate unit

---
+++ tests/test_base_check.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from typing import List
+from unittest.mock import patch, Mock
+
+from license_checks.base_checker import BaseLicenseChecker
+
+
+class SimpleLicenseChecker(BaseLicenseChecker):
+
+    def prepare_directory(self, directory: str):
+        pass
+
+    def get_license_checker_command(self) -> str:
+        return "echo 'Hello World!'"
+
+    def parse_licenses(self, output: str, configuration: dict) -> List[str]:
+        return ''
+
+
+class TestBaseLicenseChecker:
+    checker: SimpleLicenseChecker
+
+    def setup(self):
+        self.checker = SimpleLicenseChecker()
+
+    def test_remove_duplicates(self):
+        list = ['a', 'b', 'c', 'a', 'c']
+        consolidated_list = self.checker.remove_duplicates(list)
+        assert consolidated_list == ['a', 'b', 'c']
+
+    def test_get_pipenv_directories(self):
+        filenames = ['Pipfile.lock', 'Pipfile',
+                     'deployment/Pipfile', 'deployment/Pipfile.lock']
+        directories = self.checker.get_pipenv_directories(filenames)
+        assert len(directories) == 2
+        assert directories == [str(Path('.').absolute()),
+                               str(Path('.', 'deployment').absolute())]
+
+    def test_find_forbidden_licenses(self):
+        configuration = {
+            'allowed_licenses': ['BSD License', 'MIT License']
+        }
+        used_licenses = ['BSD License', 'GPL', 'MIT License']
+
+        forbidden_licenses = self.checker.find_forbidden_licenses(used_licenses, configuration)
+        assert forbidden_licenses == ['GPL']
+
+    def test_print_license_warning(self):
+        # this test is mainly run, to verify syntactic correctness
+        with TemporaryDirectory() as directory:
+            self.checker.print_license_warning(directory, [])
+
+    @patch('license_checks.base_checker.run')
+    def test_load_installed_licenses(self, run_mock):
+        result_mock = Mock()
+        result_mock.configure_mock(**{'stdout': ''})
+        run_mock.return_value = result_mock
+
+        with TemporaryDirectory() as directory:
+            self.checker.load_installed_licenses(directory, {})
+            run_mock.assert_called_once_with('echo \'Hello World!\'', capture_output=True, check=True,
+                                             cwd=directory, shell=True, text=True)
--- tests/test_pipenv.py
+++ tests/test_pipenv.py
@@ -4,58 +4,12 @@ from os.path import join
 from pathlib import Path
 from shutil import copy2
 from tempfile import TemporaryDirectory
-from unittest.mock import patch, Mock, call
+from unittest.mock import patch, call
 
 from license_checks.pipenv import PipenvLicenseChecker
 
 
 class TestPipenvLicenseChecker:
-    checker: PipenvLicenseChecker
-
-    def setup(self):
-        self.checker = PipenvLicenseChecker()
-
-    def test_remove_duplicates(self):
-        list = ['a', 'b', 'c', 'a', 'c']
-        consolidated_list = self.checker.remove_duplicates(list)
-        assert consolidated_list == ['a', 'b', 'c']
-
-    def test_get_pipenv_directories(self):
-        filenames = ['Pipfile.lock', 'Pipfile',
-                     'deployment/Pipfile', 'deployment/Pipfile.lock']
-        directories = self.checker.get_pipenv_directories(filenames)
-        assert len(directories) == 2
-        assert directories == [str(Path('.').absolute()),
-                               str(Path('.', 'deployment').absolute())]
-
-    def test_parse_licenses(self):
-        stdout = '''[
-          {
-            "License": "BSD License",
-            "Name": "starlette",
-            "Version": "0.14.1"
-          },
-          {
-            "License": "MIT License",
-            "Name": "urllib3",
-            "Version": "1.26.4"
-          },
-          {
-            "License": "BSD License",
-            "Name": "uvicorn",
-            "Version": "0.13.3"
-          },
-          {
-            "License": "MIT License",
-            "Name": "zipp",
-            "Version": "3.4.1"
-          }
-        ]
-        '''
-        configuration = {}
-        licenses = self.checker.parse_licenses(stdout, configuration)
-        assert licenses == ['BSD License', 'MIT License']
-
     DEMO_LICENSE_OUTPUT = '''[
           {
             "License": "BSD License",
@@ -85,6 +39,39 @@ class TestPipenvLicenseChecker:
         ]
         '''
 
+    checker: PipenvLicenseChecker
+
+    def setup(self):
+        self.checker = PipenvLicenseChecker()
+
+    def test_parse_licenses(self):
+        stdout = '''[
+          {
+            "License": "BSD License",
+            "Name": "starlette",
+            "Version": "0.14.1"
+          },
+          {
+            "License": "MIT License",
+            "Name": "urllib3",
+            "Version": "1.26.4"
+          },
+          {
+            "License": "BSD License",
+            "Name": "uvicorn",
+            "Version": "0.13.3"
+          },
+          {
+            "License": "MIT License",
+            "Name": "zipp",
+            "Version": "3.4.1"
+          }
+        ]
+        '''
+        configuration = {}
+        licenses = self.checker.parse_licenses(stdout, configuration)
+        assert licenses == ['BSD License', 'MIT License']
+
     def test_parse_licenses_with_excluded_packages(self):
         configuration = {
             'excluded_packages': [
@@ -93,15 +80,6 @@ class TestPipenvLicenseChecker:
         }
         licenses = self.checker.parse_licenses(self.DEMO_LICENSE_OUTPUT, configuration)
         assert licenses == ['BSD License', 'MIT License']
-
-    def test_find_forbidden_licenses(self):
-        configuration = {
-            'allowed_licenses': ['BSD License', 'MIT License']
-        }
-        used_licenses = ['BSD License', 'GPL', 'MIT License']
-
-        forbidden_licenses = self.checker.find_forbidden_licenses(used_licenses, configuration)
-        assert forbidden_licenses == ['GPL']
 
     def test_load_configuration_without_file(self):
         with TemporaryDirectory() as directory:
@@ -122,11 +100,6 @@ class TestPipenvLicenseChecker:
             configuration = self.checker.load_configuration(directory)
             assert configuration == demo_configuration
 
-    def test_print_license_warning(self):
-        # this test is mainly run, to verify syntactic correctness
-        with TemporaryDirectory() as directory:
-            self.checker.print_license_warning(directory, [])
-
     @patch('license_checks.pipenv.run')
     def prepare_directory(self, run_mock):
         run_mock.return_value = {}
@@ -137,17 +110,6 @@ class TestPipenvLicenseChecker:
                 call('pipenv install -d', check=True, cwd=directory, shell=True),
                 call("pipenv run pip install 'pip-licenses==3.3.1'", check=True, cwd=directory, shell=True),
             ])
-
-    @patch('license_checks.base_checker.run')
-    def test_load_installed_licenses(self, run_mock):
-        result_mock = Mock()
-        result_mock.configure_mock(**{'stdout': self.DEMO_LICENSE_OUTPUT})
-        run_mock.return_value = result_mock
-
-        with TemporaryDirectory() as directory:
-            self.checker.load_installed_licenses(directory, {})
-            run_mock.assert_called_once_with('pipenv run pip-licenses --format=json', capture_output=True, check=True,
-                                             cwd=directory, shell=True, text=True)
 
     def test_main_returns_failure_on_no_config(self):
         with TemporaryDirectory() as directory:
