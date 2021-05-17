54ddaca4bf56a1bab619b3f6af9c62af87cd2a58
Nicolas Byl <nicolas.byl@codecentric.de>
2021-05-17 14:06:27 +0200

    fix: add requsts library as explicit dependency

--- setup.py
+++ setup.py
@@ -5,7 +5,16 @@ from setuptools import setup
 __version__ = '1.3.0'
 
 setup(
-    install_requires=['ignore==0.1.4', 'pyyaml==5.4.1', 'texttable==1.6.3'],
+    install_requires=[
+        'certifi==2020.12.5',
+        "chardet==4.0.0; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4'",
+        "idna==2.10; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3'",
+        'ignore==0.1.4',
+        'pyyaml==5.4.1',
+        'requests==2.25.1',
+        'texttable==1.6.3',
+        "urllib3==1.26.4; python_version >= '2.7' and python_version not in '3.0, 3.1, 3.2, 3.3, 3.4' and python_version < '4'",
+    ],
     name='pre-commit-license-check',
     description='Check your repositories against a license allow list',
     url='https://github.com/nbyl/pre-commit-license-check',
