617f3b2af149cf05de1e4d858120b763151028fe
Nicolas Byl <nicolas.byl@codecentric.de>
2021-05-21 10:23:31 +0200

    refactor: create harness for integration tests

--- tests/integration/test_pipenv.py
+++ tests/integration/test_pipenv.py
@@ -1,39 +1,25 @@
 # -*- coding: utf-8 -*-
 import os
-from os.path import join
 from pathlib import Path
 from shutil import copy
 from subprocess import run
-from tempfile import TemporaryDirectory
+from typing import List
 
-from git import Repo
-
-from license_checks.configuration import Configuration
+from tests.integration.base_integration_test import IntegrationTestBase
 
 
-class TestPipenvCheck:
+class TestPipenvCheck(IntegrationTestBase):
 
-    def setup(self):
-        self.directory = TemporaryDirectory()
-        copy(join(Path(__file__).parent, 'Pipfile'), join(self.directory.name, 'Pipfile'))
+    def prepare_test_directory(self):
+        copy(Path(Path(__file__).parent, 'Pipfile').absolute(),
+             Path(self.directory.name, 'Pipfile').absolute())
         run('pipenv lock', capture_output=True, check=True, cwd=self.directory.name,
             env=dict(os.environ, PIPENV_IGNORE_VIRTUALENVS='1'), shell=True)
 
-        self.repo = Repo.init(self.directory.name)
-        self.repo.index.add(self.repo.untracked_files)
+    def get_hook_id(self) -> str:
+        return 'license-check-pipenv'
 
-    def test_run_fails_without_config(self):
-        result = self.run_pre_commit()
-        assert result.returncode == 1
-
-    def test_success(self):
-        Configuration(allowed_licenses=['MIT License'], excluded_packages=[], includes=[]).save(self.directory.name)
-        result = self.run_pre_commit()
-        print(result.stdout)
-        assert result.returncode == 0
-
-    def run_pre_commit(self):
-        return run(
-            f'pre-commit try-repo {Path(__file__).parent.parent.parent.absolute()} license-check-pipenv --all-files',
-            capture_output=True, cwd=self.directory.name,
-            env=dict(os.environ, PIPENV_IGNORE_VIRTUALENVS='1'), shell=True)
+    def get_allowed_licenses(self) -> List[str]:
+        return [
+            'MIT License'
+        ]
